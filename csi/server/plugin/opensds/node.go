61c2b5767a7b3d013fd063c1db780969fc6ea87b
PengYiOfOpenSDS <pengyi2@126.com>
2019-02-03 15:08:03 +0800

    Modify the parameters when executing the ln command

--- csi/server/plugin/opensds/node.go
+++ csi/server/plugin/opensds/node.go
@@ -305,7 +305,7 @@ func (p *Plugin) NodeStageVolume(
 			return nil, status.Error(codes.Aborted, fmt.Sprintf("failed to mkdir: %v", err.Error()))
 		}
 
-		_, err = exec.Command("ln", "-s", device, mountpoint).CombinedOutput()
+		_, err = exec.Command("ln", "-sf", device, mountpoint).CombinedOutput()
 		if err != nil {
 			return nil, status.Error(codes.Aborted, fmt.Sprintf("failed to ln: %v", err.Error()))
 		}
